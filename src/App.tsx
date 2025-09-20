78e0fc7fcf7ac6ccb3c39c6c62b7eb839f8cd5a9
Rafael <rafael.araujobazilio@gmail.com>
2025-09-19 22:57:30 -0300

    Fix: Atualizar endpoint do Formspree para envio de emails

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://formspree.io/f/xeqynpnp', {
+      const response = await fetch('https://formspree.io/f/oficinadamulta.nf@gmail.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
